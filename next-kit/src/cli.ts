eefba5db38d9faa29dfc4c78b6266edddae9840c
lumen-on <sergey.prudnikov.dev@gmail.com>
2026-06-01 20:00:27 +0000

    feat: implement component command with client/shadcn/test options

---
+++ next-kit/src/cli.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import pc from 'picocolors';
+import { initCommand } from './commands/init.js';
+import { componentCommand } from './commands/component.js';
+
+const program = new Command();
+
+program
+  .name('next-kit')
+  .description('Modern code and template generator for Next.js')
+  .version('0.1.0');
+
+program
+  .command('init')
+  .description('Initialize a new Next.js project')
+  .action(initCommand);
+
+program
+  .command('component')
+  .description('Generate a React component')
+  .argument('<name>', 'Component name')
+  .option('--client', 'Create as client component')
+  .option('--shadcn', 'Include shadcn/ui imports')
+  .option('--test', 'Generate test file')
+  .action(componentCommand);
+
+program.parse(process.argv);
